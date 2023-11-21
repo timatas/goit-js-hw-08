46be080d75cd1a1916196ef3b53a829a1ae26d61
timatas <139263028+timatas@users.noreply.github.com>
2023-11-21 21:26:52 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,13 +4,11 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const saveCurrentTime =
-  localStorage.getItem('videoplayer-current-time') ?? null;
-
-player.setCurrentTime(saveCurrentTime);
-
 const currentTime = function (data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
 };
+const saveCurrentTime = localStorage.getItem('videoplayer-current-time') || 0;
+
+player.setCurrentTime(saveCurrentTime);
 
 player.on('timeupdate', throttle(currentTime, 1000));
